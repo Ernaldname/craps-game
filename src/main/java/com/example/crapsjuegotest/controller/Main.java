9ea2e1e2fd0eda161be512aec767acf29b7cce6b
Edinson Ramirez <edinsonr547@gmail.com>
2024-03-14 19:39:23 -0500

    Icono añadido

--- src/main/java/com/example/crapsjuegotest/controller/Main.java
+++ src/main/java/com/example/crapsjuegotest/controller/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -22,7 +23,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        System.out.println("Hola reina");
-        System.out.println("H");
+        Image icon = new Image("C:\\Users\\User\\OneDrive\\Documents\\zelda_icon.png");
+        primaryStage.getIcons().add(icon);
     }
 }
